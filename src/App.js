48c0f0c3c97625234882e3b229ea5059a2191409
Jayson Lewis <jayson.lewis5@gmail.com>
2020-03-13 18:02:25 -0500

    Added Sass, bootstrap and setup header component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import './App.scss';
 
-
+class Header extends Component {
+  render(){
+    return(
+      <div>
+        <Nav>
+          <Nav.Item>
+            <Nav.Link>Home</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <h1 className=".info">Stoneking Team Realtors</h1>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Stoneking Team Realtors</h1>
-      </header>
+      <Header />
     </div>
   );
 }
