0f5acffd8d37e56ae94e5ad71e09e430dcf7fb95
Kyle Parish <kyleparish@Kyles-MacBook-Pro.local>
2020-03-24 17:21:31 -0500

    Added cards w/ images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './App.scss';
@@ -26,6 +28,7 @@ class Header extends Component {
           </Nav>
           </Navbar.Collapse>
         </Navbar>
+        
       </div>
     );
   }
@@ -36,6 +39,45 @@ class Content extends Component{
     return(
       <div>
         <h1>Body Content</h1>
+        <CardGroup>
+  <Card>
+  <Card.Img variant="top" src="../images/Sherri.jpg" />
+    <Card.Body>
+      <Card.Title>Sherri Stoneking</Card.Title>
+      <Card.Text>
+        Blah
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+  <Card.Img variant="top" src="../images/Kevin.jpg" />
+    <Card.Body>
+      <Card.Title>Kevin Stoneking</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="../images/sabrina.png" />
+    <Card.Body>
+      <Card.Title>Sabrina Kettle</Card.Title>
+      <Card.Text>
+        Blah
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>
       </div>
     );
   }
@@ -64,3 +106,4 @@ function App() {
 }
 
 export default App;
+
