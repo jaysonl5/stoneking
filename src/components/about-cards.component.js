8bfc195e37b580f0332068b2761e322e59eb354e
Kyle Parish <kyleparish@Kyles-MacBook-Pro.local>
2020-04-05 12:31:08 -0500

    tabbed cards

--- src/components/about-cards.component.js
+++ src/components/about-cards.component.js
@@ -7,43 +7,43 @@ export default class AboutCards extends Component {
         return(
             <div className="cardcontainer">
                 <CardGroup className="Allcards" >
-                <Card className="Sherri-card">
-                <Card.Img variant="top" src="../images/Sherri.jpg" />
-                <Card.Body>
-                <Card.Title>Sherri Stoneking</Card.Title>
-                <Card.Text>
-                    Blah
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-                </Card>
-                <Card>
-                <Card.Img variant="top" src="../images/Kevin.jpg" />
-                <Card.Body>
-                <Card.Title>Kevin Stoneking</Card.Title>
-                <Card.Text>
-                This card has supporting text below as a natural lead-in to additional
-                content.{' '}
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-                </Card>
-                <Card>
-                <Card.Img variant="top" src="../images/sabrina.png" />
-                <Card.Body>
-                <Card.Title>Sabrina Kettle</Card.Title>
-                <Card.Text>
-                Blah
-                </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-                </Card>
+                    <Card className="Sherri-card">
+                        <Card.Img variant="top" src="../images/Sherri.jpg" />
+                        <Card.Body>
+                        <Card.Title>Sherri Stoneking</Card.Title>
+                        <Card.Text>
+                            Blah
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="../images/Kevin.jpg" />
+                        <Card.Body>
+                        <Card.Title>Kevin Stoneking</Card.Title>
+                        <Card.Text>
+                        This card has supporting text below as a natural lead-in to additional
+                        content.{' '}
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
+                    <Card>
+                        <Card.Img variant="top" src="../images/sabrina.png" />
+                        <Card.Body>
+                        <Card.Title>Sabrina Kettle</Card.Title>
+                        <Card.Text>
+                        Blah
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                        </Card.Footer>
+                    </Card>
                 </CardGroup>
             </div>
           );
