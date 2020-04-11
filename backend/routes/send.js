25d4d0750651ff540e9b7c585bd8cf3d7bb89e58
Jayson Lewis <jayson.lewis5@gmail.com>
2020-04-11 12:28:03 -0500

    Added nodemailer function to contact form

---
+++ backend/routes/send.js
@@ -0,0 +1,68 @@
+const router = require('express').Router();
+var nodemailer = require('nodemailer');
+require('dotenv').config();
+
+router.route('/send').post((req, res) => {
+
+    var transport = {
+        host: 'smtp.gmail.com',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
+        }
+    }
+
+    var firstName = req.body.firstName
+    var lastName = req.body.lastName
+    var fullName = firstName + ` ` + lastName;
+    var email = req.body.email
+    var buySell = req.body.buySell
+    var propertyType = req.body.propertyType
+    var address = req.body.address
+    var city = req.body.city
+    var state = req.body.state
+    var zip = req.body.zip
+    var buyDescription = req.body.buyDescription
+    var content = `
+        Name: ${fullName} \n 
+        email: ${email} \n 
+        Looking to: ${buySell} \n
+        Property Type: ${propertyType} \n
+        Address: ${address} \n
+        City: ${city} \n
+        State: ${state} \n
+        Zip: ${zip} \n
+        Description: ${buyDescription} \n
+        `
+
+    var mail = {
+        from: fullName,
+        to: 'jayson.lewis5@gmail.com',
+        subject: 'Stoneking Contact Request!',
+        text: content
+    }
+
+    var transporter = nodemailer.createTransport(transport);
+
+    transporter.verify((error, success) => {
+        if(error) {
+            console.log(error);
+        } else {
+            console.log('Server is ready to take messages.')
+        }
+    })
+
+    transporter.sendMail(mail, (err, data) => {
+        if (err) {
+            res.json({
+                msg: 'fail'
+            })
+        } else {
+            res.json({
+                msg: 'success'
+            })
+        }
+    })
+})
+
+module.exports = router;
