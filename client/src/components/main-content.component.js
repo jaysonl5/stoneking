fa29cc870fb75b6f8d1f7e408df1121bdce153fe
Jayson Lewis <jayson.lewis5@gmail.com>
2020-10-03 15:35:57 -0500

    slight code formatting

--- client/src/components/main-content.component.js
+++ client/src/components/main-content.component.js
@@ -5,35 +5,23 @@ import ContactForm from './contact-form.component.js';
 export default function MainContent(props){
     return(
         <div>
-               
-                    <h2 className="serveBanner">Here to serve you</h2>
-
+            <h2 className="serveBanner">Here to serve you</h2>
+            
                <div className="row">
-"
-                    
-
                     <div className="col-3-of-4">
-                    
                         <p className="mainBody">
-                        
                             Stoneking Real Estate Team is a full service real estate team that is dedicated to providing outstanding service to all our 
                             clients in the Oklahoma City metro and all surrounding suburbs. Our goal is to help you find a place to share your life and make memories with the people you love. Contact us today to find the home of your dreams!
                         </p>
-
-                        
                     </div>
 
                     <div className="col-3-of-4"> 
                         <img src="../images/family_at_home.jpg" className="imgFamily" alt="Mother and child enjoying their home."></img>
                     </div>
-
                     
-
                     <div className="col-1-of-4">
-                    <ContactForm />
-                    
+                        <ContactForm />
                     </div>
-                    
                 </div>
 
                 <div className="row">
